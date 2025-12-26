a0d8c64b26660de35b5ed89357311e2cb1489731
waseem-baig <waseem20032005@gmail.com>
2025-12-26 06:58:31 +0000

    JBSUGUGDSJBD

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:8080",
       "http://localhost:3000",
+      "https://www.srikarthikaenterprises.in",
       process.env.CLIENT_URL,
     ].filter(Boolean),
     credentials: true, // Allow cookies to be sent
